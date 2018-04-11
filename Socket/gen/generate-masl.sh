772eeec36a8c0a7da927bac3a47866e0b4e5ebe1
Levi Starrett <levi@roxsoftware.com>
2018-04-11 13:25:27 -0400

    clean up and update Socket code generation

--- Socket/gen/generate-masl.sh
+++ Socket/gen/generate-masl.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+MASLMC=$HOME/git/masl/core-java/install/masl-core/bin/masl-core
 CWD=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 TEST=-test
 if [[ $# > 0 && $1 == "notest" ]]; then
   TEST=
 fi
-$HOME/git/masl/core-java/install/masl-core/bin/masl-core -domainpath $CWD/masl/Socket:$GEN_Socket_Logger_LOC:$GEN_Socket_Format_LOC -mod $CWD/masl/Socket/Socket.mod -output $CWD/gen/code_generation/Socket $TEST
+$MASLMC -mod $CWD/masl/Socket/Socket.mod -output $CWD/gen/code_generation $TEST -copyright $CWD/../LICENSE.md -custombuildfile $CWD/gen/custom.cmake
 cmake $CWD -DCMAKE_INSTALL_PREFIX=$PWD
