5d314b594f2e356aa47897fe08dc2a816e9d3347
Levi Starrett <levi@roxsoftware.com>
2018-04-19 09:39:27 -0400

    introduced BinaryIO dependency

--- Telnet/gen/generate-masl.sh
+++ Telnet/gen/generate-masl.sh
@@ -5,12 +5,13 @@ if [[ "" == $GEN_Telnet_Socket_LOC ]]; then
   GEN_Telnet_Socket_LOC="$CWD/../Socket"
 fi
 GEN_Telnet_Logger_LOC="$HOME/git/masl/examples/utils/Logger_OOA"
+GEN_Telnet_BinaryIO_LOC="$HOME/git/masl/examples/utils/BinaryIO_OOA"
 TEST=-test
 if [[ $# > 0 && $1 == "notest" ]]; then
   TEST=
 fi
 
-$MASLMC -domainpath $GEN_Telnet_Socket_LOC/masl/Socket:$GEN_Telnet_Logger_LOC \
+$MASLMC -domainpath $GEN_Telnet_Socket_LOC/masl/Socket:$GEN_Telnet_Logger_LOC:$GEN_Telnet_BinaryIO_LOC \
         -mod $CWD/masl/Telnet/Telnet.mod \
         -output $CWD/gen/code_generation/Telnet \
         -copyright $CWD/../LICENSE.md \
@@ -23,6 +24,10 @@ $MASLMC -mod $GEN_Telnet_Socket_LOC/masl/Socket/Socket.int \
         -skiptranslator Sqlite
 $MASLMC -mod $GEN_Telnet_Logger_LOC/Logger.int \
         -output $CWD/gen/code_generation/Logger \
+        -copyright $CWD/../LICENSE.md \
+        -skiptranslator Sqlite
+$MASLMC -mod $GEN_Telnet_BinaryIO_LOC/BinaryIO.int \
+        -output $CWD/gen/code_generation/BinaryIO \
         -copyright $CWD/../LICENSE.md \
         -skiptranslator Sqlite
 
